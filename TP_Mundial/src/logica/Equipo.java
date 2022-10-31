389e2bba458e33a6c3f433c4ab573d84ddddfa1f
escuela@davinci.edu.ar <xxxrigobertoxxx@gmail.com>
2022-10-31 15:55:17 -0300

    Primer commit
    Cree el archivo xd

---
+++ TP_Mundial/src/logica/Equipo.java
@@ -0,0 +1,43 @@
+package logica;
+
+public class Equipo {
+
+	private int id_equipo;
+	private String nombre;
+	private int goles;
+	private int grupo;
+	
+	public Equipo(int id_equipo, String nombre, int grupo) {
+		super();
+		this.id_equipo = id_equipo;
+		this.nombre = nombre;
+		this.goles = 0;
+		this.grupo = grupo;
+	}
+	public int getId_equipo() {
+		return id_equipo;
+	}
+	public void setId_equipo(int id_equipo) {
+		this.id_equipo = id_equipo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getGoles() {
+		return goles;
+	}
+	public void setGoles(int goles) {
+		this.goles = goles;
+	}
+	public int getGrupo() {
+		return grupo;
+	}
+	public void setGrupo(int grupo) {
+		this.grupo = grupo;
+	}
+	
+	
+}
